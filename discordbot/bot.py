53491b99226e9c5e213669b7e5149014cbb40421
Cleyton Souza Martins <122110138+CleytonSM@users.noreply.github.com>
2023-03-28 12:22:06 -0300

    adding on_member_join()

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -9,9 +9,13 @@ intents.message_content = True
 
 client = commands.Bot(command_prefix = '!', intents=intents)
 
-
 @client.event
 async def on_ready():
     print(f'We have logged on as {client.user}')
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(725848521449930816)
+    await channel.send(f'Guten morgen {member.mention}.')
 
 client.run(BOTTOKEN.bottoken)
