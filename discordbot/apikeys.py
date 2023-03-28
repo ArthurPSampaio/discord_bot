fb3a76605156fb929585c7a77f7d1575750915da
Cleyton Souza Martins <122110138+CleytonSM@users.noreply.github.com>
2023-03-28 11:33:36 -0300

    deleting apikey.py

---
+++ discordbot/apikeys.py
@@ -0,0 +1,7 @@
+class bot:
+    def __init__(self, bottoken) -> None:
+        self.bottoken = bottoken
+
+BOTTOKEN = bot('MTA5MDI3NDM3NzExMjc2MDQyMQ.G4qLKs.FTaOT7KMGrKDAP6HVGJNNqPFIqV-5bGtlIfhjQ')
+
+print(BOTTOKEN.bottoken)
