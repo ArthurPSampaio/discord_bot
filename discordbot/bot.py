fb5a75d2e8af7a4f487a2d3a399089987ad5b108
ArthurPSampaio <113519439+ArthurPSampaio@users.noreply.github.com>
2023-03-28 17:30:32 -0300

    Adição do ChatGPT

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -1,5 +1,7 @@
 import discord
+import openai
 from discord.ext import commands
+import requests
 
 from apikeys import *
 
@@ -8,6 +10,7 @@ intents.members = True
 intents.message_content = True
 
 client = commands.Bot(command_prefix = '!', intents=intents)
+# bot = commands.Bot(command_prefix='!', intents=intents)
 
 @client.event
 async def on_ready():
@@ -17,5 +20,31 @@ async def on_ready():
 async def on_member_join(member):
     channel = client.get_channel(725848521449930816)
     await channel.send(f'Guten morgen {member.mention}.')
+
+
+@client.command(name='chat')
+async def chat_command(ctx, *, message):
+    # verificar se o autor da mensagem é o bot
+    if ctx.author.bot:
+        return
+
+    # enviar mensagem de saudação
+    # await ctx.send(f'Olá, eu sou um bot de chat! Você disse: {message}')
+
+    # chamar a API do ChatGPT
+    openai.api_key = openai_API
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=f"Conversa: {message}\nBot:",
+        temperature=0.7,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        timeout=15,
+    )
+
+    # enviar resposta do ChatGPT
+    await ctx.send(response.choices[0].text)
+
 
 client.run(BOTTOKEN.bottoken)
