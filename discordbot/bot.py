464f041e85ea95324abb6748420145ae7d7fe06b
Cleyton Souza Martins <122110138+CleytonSM@users.noreply.github.com>
2023-03-28 11:25:32 -0300

    Finishing apikey.py

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -1,12 +1,13 @@
 import discord
 from discord.ext import commands
 
+from apikey import *
+
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
 client = commands.Bot(command_prefix = '!', intents=intents)
 
-client.run('bot token')
-print('teste123123123123')
-print('teste123123123123')
+client.run(BOTTOKEN.bottoken)
+
