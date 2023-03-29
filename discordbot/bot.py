9e0e88da76e67e068f223c9e42ed6b86297af598
ArthurPSampaio <113519439+ArthurPSampaio@users.noreply.github.com>
2023-03-29 15:49:35 -0300

    Adding image generator feature

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -3,6 +3,7 @@ import openai
 import asyncio
 from discord.ext import commands
 import requests
+import json
 
 from apikeys import *
 
@@ -10,7 +11,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-# client = commands.Bot(command_prefix = '!', intents=intents)
+# bot = commands.Bot(command_prefix = '!', intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
@@ -23,7 +24,7 @@ async def on_member_join(member):
     await channel.send(f'Guten morgen {member.mention}.')
 
 @bot.command()
-async def gpt(ctx: commands.context, *, prompt: str):
+async def gpt(ctx: commands.Context, *, prompt: str):
     # verificar se o autor da mensagem é o bot
     if ctx.author.bot:
         return
@@ -55,5 +56,29 @@ async def leave(ctx):
         channel = ctx.guild.voice_client
         await channel.disconnect()
 
+@bot.command()
+async def img(ctx, *, description):
+    # Parâmetros da solicitação HTTP
+    data = {
+        "model": "image-alpha-001",
+        "prompt": description,
+        "num_images": 1,
+        "size": "1024x1024"
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai_API}"
+    }
+
+    # Enviar a solicitação HTTP
+    response = requests.post("https://api.openai.com/v1/images/generations", data=json.dumps(data), headers=headers)
+
+    # Obter a URL da imagem gerada
+    image_url = response.json()["data"][0]["url"]
+
+    # Enviar a imagem para o canal do Discord
+    embed = discord.Embed(title="Imagem gerada com base na descrição de texto:", description=description)
+    embed.set_image(url=image_url)
+    await ctx.send(embed=embed)
 
 bot.run(BOTTOKEN.bottoken)
