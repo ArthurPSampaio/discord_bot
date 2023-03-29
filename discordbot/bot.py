f0fc4996ab55880f6b338c5773027ce5e40e1afc
ArthurPSampaio <113519439+ArthurPSampaio@users.noreply.github.com>
2023-03-29 10:16:15 -0300

    Fixing ChatGPT API

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -1,7 +1,6 @@
 import discord
 import openai
 from discord.ext import commands
-import requests
 
 from apikeys import *
 
@@ -21,28 +20,24 @@ async def on_member_join(member):
     channel = bot.get_channel(725848521449930816)
     await channel.send(f'Guten morgen {member.mention}.')
 
-@bot.command(name='chat')
-async def chat_command(ctx, *, message):
+@bot.command()
+async def gpt(ctx: commands.context, *, prompt: str):
     # verificar se o autor da mensagem é o bot
     if ctx.author.bot:
         return
-    # enviar mensagem de saudação
-    await ctx.send(f'Olá, eu sou um bot de chat! Você disse: {message}')
-    # chamar a API do ChatGPT
     openai.api_key = openai_API
     response = openai.Completion.create(
-        engine="davinci",
-        prompt=f"Bot:",
-        temperature=0.7,
-        max_tokens=100,
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=200,
         n=1,
         stop=None,
         timeout=15,
     )
-
-    # enviar resposta do ChatGPT
-    await ctx.send(response.choices[0].text)
-
+    # enviar resposta do ChatGPT em uma embed
+    embed = discord.Embed(title="Resposta:", description=response.choices[0].text, color=0x00ff00)
+    await ctx.send(embed=embed)
 
 
 bot.run(BOTTOKEN.bottoken)
