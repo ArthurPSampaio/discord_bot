6aac6271ba4c9be88ae6e8ff80c5baad3461b6ed
Cleyton Souza Martins <122110138+CleytonSM@users.noreply.github.com>
2023-04-24 12:40:42 -0300

    finishing main champ getter

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -83,40 +83,47 @@ async def img(ctx, *, prompt: str):
     await ctx.reply(files=images1)  # dá reply na mensagem do usuário com as 9 imagens geradas
 
 
+
+
 @bot.command()
 async def lol(ctx, *, player):
     api_key = league_of_legendsAPI
     watcher = LolWatcher(api_key)
     my_region = 'br1'
     try:
-
+        
         status = watcher.summoner.by_name(my_region, player)
         ranked_status_player = watcher.league.by_summoner(my_region, status['id'])
-        champion_mastery = watcher.champion_mastery.by_summoner(my_region, status['id'])
-        solo_duo = 'RANKED_SOLO_5x5'
-        solo_duo_status = watcher.league.entries(my_region, solo_duo, 'GOLD', 'I', 1) + api_search
-        print(status)
-        print('-------')
-        print(ranked_status_player[0]) # [0] get into to the dic and show player's info
-        # the error was: it was necessary to access the list first, then manipulate the dict
+        champid = watcher.champion_mastery.by_summoner(my_region, status['id']) #retorna 240(id) -> kled
+        latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion'] #get lol's last version about 'n' and about the champions
+        static_champion_list = watcher.data_dragon.champions(latest, False, 'pt_BR') #get champions static info
+
+        champ_key = champid[0]['championId']
+   
         embed = discord.Embed(title=status['name'])
 
         queueType = ranked_status_player[0].get('queueType')
-        # ! needs study the api's riot to know how to get RANKED_FLEX_SR at a player who have RANKED_SOLO_5X5 status
+        print(queueType)
         def players_info():
             embed.add_field(inline=True, name='Wins ', value=str(ranked_status_player[0]['wins']))
             embed.add_field(inline=True, name='Losses ', value=str(ranked_status_player[0]['losses']))
             embed.add_field(inline=True, name='Pdls ', value=str(ranked_status_player[0]['leaguePoints']))
             embed.add_field(inline=True, name='Tier ', value=str(ranked_status_player[0]['tier']))
             embed.add_field(inline=True, name='Rank ', value=str(ranked_status_player[0]['rank']))
-        
-        if queueType == 'RANKED_FLEX_SR':
-            embed.add_field(name='No Matches', value=("This player doesn't have solo/duo matches already"))
-        elif queueType == 'RANKED_SOLO_5x5':
+            champion_name = next((champ['name'] for champ in static_champion_list['data'].values() if champ['key'] == str(champ_key)), None)
+            '''
+            champ['name'] is getting the value of name in static_champion_list (dictionary).
+            So when 'if' starts, the id from static_champion_list stored on champ will pass through an equal test, confirming if champ['key'] is equal to champions key.
+            '''
+            print(static_champion_list['data'].values())
+            if champion_name:
+                embed.add_field(inline=False, name="Main Champ", value=champion_name)
+                
+        if ranked_status_player:
             players_info()
-            
         else:
-            await ctx.send('Player not found')
+            embed.add_field(name='No Matches', value=("This player doesn't have any ranked matches yet"))
+            players_info()
         await ctx.send(embed=embed)
     except ApiError as e:
         await ctx.send(f'Error: {e}')
